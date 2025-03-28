f21a7b26ebfdbe5c5f9e140e4013faebc8e02e73
Aidan <scorpio.knowles@gmail.com>
2025-03-28 14:14:51 +0200

    Remove unused components and add Profile and Notification screens

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import { styles } from "../styles/auth.styles";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Link href={"/notification"}>visit notification screen</Link>
+    </View>
+  );
+}
